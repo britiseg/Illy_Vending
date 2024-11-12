fcd6622ab065d38b234e6afbd7a5508a052f68cf
Marko nava <mnava@prepango.com>
2024-11-12 17:56:35 -0600

    Adding sections, illy experience and new

---
+++ resources/views/experience/main.blade.php
@@ -0,0 +1,59 @@
+<div class="container py-4 mx-auto">
+    <div class="text-center mb-8">
+        <span class="text-center justify-center align-center mx-auto mb-2 inline" style="font-size: 36px;line-height: 125%;">Discover What Makes
+            <p class="mt-4 font-bold text-red-600 inline" style="font-style: normal;font-weight: 800; font-size: 42px;line-height: 150%;">
+        illy </p> an Exceptional Partner
+        </span>
+    </div>
+    <div class="grid grid-cols-1 sm:grid-cols-2 gap-4 mt-2" style="background: #F6F0EB">
+
+        <div class="flex flex-col text-left">
+            <p class="mt-4 font-bold text-red-600" style="font-style: normal;font-weight: 800; font-size: 28px;line-height: 150%;">
+                Our Goal is to Delight Coffee Lovers
+            </p>
+            <p class="text-2xl mt-4 mb-1 text-gray-700">
+                illy aims to be the reference standard for coffee excellence and culture worldwide, an innovative company that offers the best products and places to enjoy them, and thanks to this grows and becomes a leader in high-end coffee.
+            </p>
+            <p class="mt-4 font-bold text-red-600" style="font-style: normal;font-weight: 800; font-size: 28px;line-height: 150%;">
+                We are serious about sustainability
+            </p>
+            <p class="text-2xl mt-4 mb-1 text-gray-700">
+                illy has been carbon neutral since 2023 after creating the “One Makes the Difference” initiative to demonstrate how sustainable quality helps protect and improve the well being of the planet. It reminds consumers that everyone’s actions can make a difference.                </p>
+        </div>
+
+        <div class="flex justify-center p-2 mt-4">
+            <img src="{{ asset('images/experience/frame.png') }}" alt="Coffee" class="w-full h-64 md:h-80 lg:h-96 object-cover">
+        </div>
+    </div>
+
+    <div class="grid grid-cols-1 sm:grid-cols-2 gap-4 mt-2" style="background: #F6F0EB">
+
+        <div class="flex flex-col text-left">
+            <p class="mt-4 font-bold text-gray-700" style="font-style: normal;font-weight: 800; font-size: 28px;line-height: 150%;">
+                THE DIRECT TRADE MODEL.
+            </p>
+            <p class="text-2xl mt-4 mb-1 text-gray-700">
+                Every single bean we harvest is 100% traceable to the source. We purchase our coffee directly from more than 800 growers, paying on average 30% more for the highest-quality Arabica beans. We possess a 100% sustainable supply chain.                </p>
+            <p class="mt-4 font-bold text-gray-700" style="font-style: normal;font-weight: 800; font-size: 28px;line-height: 150%;">
+                UNIVERSITY OF COFFEE
+            </p>
+            <p class="text-2xl mt-4 mb-1 text-gray-700">
+                We prioritize environmentally conscious practices. The establishment of the University of Coffee, ultra-low emission coffee roasting plants, and partnerships with universities to research sustainable coffee production. We pursue sustainability by sharing the value it generates (economic sustainability), personal growth (social sustainability) and respect for the ecosystem (environmental sustainability)
+            </p>
+            <button type="submit" class="w-3/4 p-2 rounded-full bg-red-600 mx-auto text-white font-bold"> Get In Touch</button>
+        </div>
+
+        <div class="flex justify-center p-4 mt-16">
+            <img src="{{ asset('images/experience/frame.png') }}" alt="Coffee" class="w-full h-64 md:h-80 lg:h-96 object-cover">
+        </div>
+    </div>
+
+    <div class="grid grid-cols-1 sm:grid-cols-2 gap-4 mt-2" style="background: #F6F0EB">
+        <div class="mb-2">
+            <img src="images/experience/uno.png" alt="">
+        </div>
+        <div class="mb-2">
+            <img src="images/experience/dos.png" alt="">
+        </div>
+    </div>
+</div>
---
+++ resources/views/new/main.blade.php
@@ -0,0 +1,51 @@
+<div class="container py-4 mx-auto">
+
+    <div class="grid grid-cols-1 lg:grid-cols-2 gap-4 mt-2" style="background: #F6F0EB">
+        <div class="flex flex-col">
+            <img src="{{ asset('images/new/new.png') }}" alt="Coffee" class="object-cover">
+        </div>
+        <div class="text-center mb-8">
+            <span class="text-center justify-center align-center mx-auto mb-2 inline" style="font-size: 36px;line-height: 125%;">Discover What Makes
+                The New, ‘Ready to Drink’, Cold Brew Cans Vending Machine
+            </span>
+            <div class="grid grid-cols-1 lg:grid-cols-2 gap-4 mt-2">
+                <div class="flex flex-col text-left">
+                    <p class="text-2xl mt-4 mb-1 text-gray-700">
+                        These ready to drink beverages are expertly crafted to offer coffee lovers a refreshing way to enjoy coffee anywhere they want it. The unit features all three illy cold brew varieties in convenient, on-the-go, 8.5oz slim cans. Pick from a wide selection of espresso beverages, delivered quickly and conveniently.
+                    </p>
+                    <div class="mb-2">
+                        <img src="images/new/FrameA.png" alt="">
+                    </div>
+                </div>
+                <div class="flex flex-col text-left">
+                    <p class="text-2xl mt-4 mb-1 text-gray-700">
+                        This machine features a modern visual display, 9” color touchscreen and full motion video. It offers product information, integrated cashless payment, multi-product purchases from a single transaction and digital advertising
+                    </p>
+                    <div class="mb-2">
+                        <img src="images/new/FrameB.png" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="text-left mb-8">
+        <p class="mt-4 font-bold text-gray-700" style="font-style: normal;font-weight: 800; font-size: 28px;line-height: 150%;">
+            Our Premium Imported Cold Brews
+        </p>
+    </div>
+    <div class="grid grid-cols-1 sm:grid-cols-3 gap-4 mt-2" style="background: #F6F0EB">
+        <div class="card">
+            <div style="align-self: stretch; color: #F80000; font-size: 20px; font-family: Manrope; font-weight: 800; text-transform: uppercase; line-height: 30px; word-wrap: break-word">CLASSIC COLD BREW</div>
+            <div style="align-self: stretch; color: black; font-size: 20px; font-family: Manrope; font-weight: 400; line-height: 30px; word-wrap: break-word">Intense coffee aroma enriched with sweet notes of chocolate and caramel</div>
+        </div>
+        <div class="card">
+            <div style="align-self: stretch; color: #F80000; font-size: 20px; font-family: Manrope; font-weight: 800; text-transform: uppercase; line-height: 30px; word-wrap: break-word">CAPPUCCINO COLD BREW</div>
+            <div style="align-self: stretch; color: black; font-size: 20px; font-family: Manrope; font-weight: 400; line-height: 30px; word-wrap: break-word">Intense coffee aroma enriched with sweet notes of chocolate and caramel</div>
+        </div>
+        <div class="card">
+            <div style="align-self: stretch; color: #F80000; font-size: 20px; font-family: Manrope; font-weight: 800; text-transform: uppercase; line-height: 30px; word-wrap: break-word">LATTE macchiato cold brew</div>
+            <div style="align-self: stretch; color: black; font-size: 20px; font-family: Manrope; font-weight: 400; line-height: 30px; word-wrap: break-word">Intense coffee aroma enriched with sweet notes of chocolate and caramel</div>
+        </div>
+    </div>
+</div>
