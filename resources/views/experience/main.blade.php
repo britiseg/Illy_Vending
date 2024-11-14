772cd4c2a0913f2910712fc0ff2f5a90dcc750a4
Marko nava <mnava@prepango.com>
2024-11-14 11:57:47 -0600

    Adding version v3 to new and experience

--- resources/views/experience/main.blade.php
+++ resources/views/experience/main.blade.php
@@ -1,23 +1,23 @@
 <div class="container py-4 mx-auto">
     <div class="text-center mb-8">
-        <span class="text-center justify-center align-center mx-auto mb-2 inline" style="font-size: 36px;line-height: 125%;">Discover What Makes
-            <p class="mt-4 font-bold text-red-600 inline" style="font-style: italic;font-weight: 800; font-size: 42px;line-height: 150%;">
-        illy </p> an Exceptional Partner
-        </span>
+        <div class="flex justify-center p-2 mt-4">
+            <img src="{{ asset('images/experience/label.png') }}" class="hidden lg:block" alt="label">
+            <img src="{{ asset('images/experience/label_mobile.png') }}" class="block lg:hidden" alt="label">
+        </div>
     </div>
     <div class="grid grid-cols-1 sm:grid-cols-2 gap-4 mt-2" style="background: #F6F0EB">
 
         <div class="flex flex-col text-left">
-            <p class="mt-4 font-bold text-red-600" style="font-style: normal;font-weight: 800; font-size: 28px;line-height: 150%;">
+            <h1 class="header_experience_red">
                 Our Goal is to Delight Coffee Lovers
-            </p>
-            <p class="text-2xl mt-4 mb-1 text-gray-700">
+            </h1>
+            <p class="text-lg mt-4 mb-1 text-gray-700">
                 illy aims to be the reference standard for coffee excellence and culture worldwide, an innovative company that offers the best products and places to enjoy them, and thanks to this grows and becomes a leader in high-end coffee.
             </p>
-            <p class="mt-4 font-bold text-red-600" style="font-style: normal;font-weight: 800; font-size: 28px;line-height: 150%;">
+            <h1 class="header_experience_red">
                 We are serious about sustainability
-            </p>
-            <p class="text-2xl mt-4 mb-1 text-gray-700">
+            </h1>
+            <p class="text-lg mt-4 mb-1 text-gray-700">
                 illy has been carbon neutral since 2023 after creating the “One Makes the Difference” initiative to demonstrate how sustainable quality helps protect and improve the well being of the planet. It reminds consumers that everyone’s actions can make a difference.                </p>
         </div>
 
@@ -29,15 +29,15 @@
     <div class="grid grid-cols-1 sm:grid-cols-2 gap-4 mt-2" style="background: #F6F0EB">
 
         <div class="flex flex-col text-left">
-            <p class="mt-4 font-bold text-gray-700" style="font-style: normal;font-weight: 800; font-size: 28px;line-height: 150%;">
+            <h1 class="header_experience_red">
                 THE DIRECT TRADE MODEL.
-            </p>
-            <p class="text-2xl mt-4 mb-1 text-gray-700">
+            </h1>
+            <p class="text-lg mt-4 mb-1 text-gray-700">
                 Every single bean we harvest is 100% traceable to the source. We purchase our coffee directly from more than 800 growers, paying on average 30% more for the highest-quality Arabica beans. We possess a 100% sustainable supply chain.                </p>
-            <p class="mt-4 font-bold text-gray-700" style="font-style: normal;font-weight: 800; font-size: 28px;line-height: 150%;">
+            <p class="header_experience_red">
                 UNIVERSITY OF COFFEE
             </p>
-            <p class="text-2xl mt-4 mb-1 text-gray-700">
+            <p class="text-lg mt-4 mb-1 text-gray-700">
                 We prioritize environmentally conscious practices. The establishment of the University of Coffee, ultra-low emission coffee roasting plants, and partnerships with universities to research sustainable coffee production. We pursue sustainability by sharing the value it generates (economic sustainability), personal growth (social sustainability) and respect for the ecosystem (environmental sustainability)
             </p>
             <button type="submit" class="w-3/4 p-2 rounded-full bg-red-600 mx-auto text-white font-bold"> Get In Touch</button>
