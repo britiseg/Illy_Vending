91bc66cf394832310e220cab9b7b0b771001f0ba
Brigette Gutierrez <britise@gmail.com>
2024-11-21 16:34:05 -0600

    adjust1

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,19 +1,17 @@
 
-<div class="relative pX-4 ">
-    <div class="flex items-center justify-between">
-        <div class="flex items-center space-x-2 !mb-0.5 !ml-2">
-            <img src="{{ asset('images/logo_prepango.png') }}" alt="Logo Prepango" class="h-8 !mt-0.5">
-            <img src="{{ asset('images/logo_illy.png') }}" alt="Logo Illy" class="h-8 !mt-0.5">
+
+<div id="header" class="fixed top-0 left-0 w-full bg-white shadow-md z-20">
+    <div class="flex items-center justify-between p-0">
+        <div class="flex items-center space-x-2">
+            <img src="{{ asset('images/logo_prepango.png') }}" alt="Logo Prepango" class="h-8">
+            <img src="{{ asset('images/logo_illy.png') }}" alt="Logo Illy" class="h-8">
         </div>
-        <div>
-            <button type="button" class="inline-flex items-center bg-primary-red text-white font-semibold px-3 py-2 text-sm shadow-sm hover:bg-secondary-red hover:shadow-lg focus:bg-secondary-red focus:shadow-lg focus:outline-none focus:ring-0 active:bg-primary-red active:shadow-lg">
-                Get In Touch
-            </button>
-        </div>
+        <button type="button" class="bg-primary-red text-white font-semibold px-3 py-2 text-sm shadow-sm hover:bg-secondary-red focus:outline-none">
+            Get In Touch
+        </button>
     </div>
 </div>
-
-<div id="navbar" class="fixed top-0 left-0 right-0 bg-med_beige py-4 px-6 shadow-md transition-all duration-300 ease-in-out z-10 hidden-navbar">
+<div id="navbar" class="fixed top-[34px] left-0 right-0 bg-med_beige py-4 px-6 shadow-md transition-all duration-300 ease-in-out z-20 hidden-navbar">
     <nav class="flex space-x-6 justify-left md:justify-center">
         <a href="#" class="nav-item font-bold" onclick="selectNavItem(event)">The Prepango Program</a>
         <a href="#" class="nav-item" onclick="selectNavItem(event)">The Illy Experience</a>
@@ -33,6 +31,8 @@
  <div class="flex justify-center">
         <img src="{{ asset('images/program/machine_1.jpeg') }}" alt="Coffee Machine" class="w-full h-auto lg:h-96 object-cover shadow-md">
     </div>
+
+
     <div class="w-full bg-med_beige py-4 px-6 nav-bar overflow-x-auto scrollbar-hide" id="navBar" onscroll="toggleArrows()">
         <nav class="flex space-x-6 justify-left md:justify-center">
             <a href="#prepango-program" class="nav-item font-bold" onclick="selectNavItem(event)">The Prepango Program</a>
@@ -50,9 +50,6 @@
     </button>
 </div>
 
-<button type="button" id="btn-get-in-touch" class="fixed bottom-5 left-5 hidden rounded-full bg-primary-red p-3 text-xs font-medium uppercase leading-tight text-white shadow-md transition duration-150 ease-in-out hover:bg-secondary-red hover:shadow-lg focus:bg-secondary-red focus:shadow-lg focus:outline-none focus:ring-0 active:bg-primary-red active:shadow-lg">
-    Get in Touch
-</button>
 
 <button type="button" data-twe-ripple-init data-twe-ripple-color="light" class="!fixed bottom-5 end-5 hidden rounded-full bg-dark_brown p-3 text-xs font-medium uppercase leading-tight text-white shadow-md transition duration-150 ease-in-out hover:bg-med_brown hover:shadow-lg focus:bg-med_brown focus:shadow-lg focus:outline-none focus:ring-0 active:bg-dark_brown active:shadow-lg" id="btn-back-to-top">
     <span class="[&>svg]:w-4">
