2283ffad3eac827374b347dfbadd019656d2ee98
Marko nava <mnava@prepango.com>
2024-12-11 10:23:46 -0600

    Cambios finales

--- resources/views/experience/main.blade.php
+++ resources/views/experience/main.blade.php
@@ -1,68 +1,76 @@
-<div class="container py-8 px-6">
-
-    <div class="flex flex-col md:flex-row items-center w-full  justify-center text-center gap-4">
-        <div class="flex flex-col md:flex-row md:items-center gap-2 md:gap-4">
-
-            <div class="flex items-center gap-2">
-                <div class="header_1   md:mb-0">See What Makes </div>
-                <div class="w-12 h-[42px] md:w-[83px] md:h-[73px] bg-no-repeat bg-center bg-contain" style="background-image: url('/images/illy_red.svg');"></div>
-
-            </div>
-            <div class="header_1 gap-2">an Exceptional Partner</div>
-        </div>
-    </div>
-
-    <div class="grid grid-cols-1 sm:grid-cols-2 gap-4 mt-2" style="background: #F6F0EB">
-
-        <div class="flex flex-col text-left">
-            <h1 class="header_red responsive-padding !pt-0 !pb-3">
-                Our Goal is to Delight Coffee Lovers
-            </h1>
-            <p class="body_txt responsive-padding !pt-0 md:pl-10 !pb-3">
-                illy aims to be the reference standard for coffee excellence and culture worldwide, an innovative company that offers the best products and places to enjoy them, and thanks to this grows and becomes a leader in high-end coffee.
-            </p>
-            <h1 class="header_red responsive-padding !pt-0 !pb-3">
-                We are serious about sustainability
-            </h1>
-            <p class="body_txt responsive-padding !pt-0 md:pl-10 !pb-3">
-                illy has been carbon neutral since 2023 after creating the “One Makes the Difference” initiative to demonstrate how sustainable quality helps protect and improve the well being of the planet. It reminds consumers that everyone’s actions can make a difference.                </p>
-        </div>
-
+<div class="container py-4 mx-auto" style="background: #F6F0EB">
+    <div class="mb-8 text-center">
         <div class="flex justify-center p-2 mt-4">
-            <img src="{{ asset('images/experience/image_one.png') }}" alt="Coffee" class="w-full h-56 md:h-80 lg:h-72 object-cover">
+            <img src="{{ asset('images/experience/label_illy.png') }}" class="hidden lg:block" alt="label">
+            <img src="{{ asset('images/experience/label_illy_mobile.png') }}" class="block lg:hidden" alt="label">
         </div>
     </div>
-
-    <div class="grid grid-cols-1 sm:grid-cols-2 gap-4" style="background: #F6F0EB">
+    <div class="grid grid-cols-1 gap-4 mt-2 sm:grid-cols-2" style="background: #F6F0EB">
 
         <div class="flex flex-col text-left">
-            <h1 class="header_red responsive-padding !pt-0 !pb-3">
-                THE DIRECT TRADE MODEL.
+            <h1 class="header_experience_red">
+                THE DIRECT TRADE MODEL
             </h1>
-            <p class="body_txt responsive-padding !pt-0 md:pl-10 !pb-3">
-                Every single bean we harvest is 100% traceable to the source. We purchase our coffee directly from more than 800 growers, paying on average 30% more for the highest-quality Arabica beans. We possess a 100% sustainable supply chain.                </p>
-            <p class="header_red responsive-padding !pt-0 !pb-3">
-                UNIVERSITY OF COFFEE
+            <p class="mt-4 mb-1 text-lg text-gray-700">
+                Every single bean we harvest is 100% traceable to the source. We purchase our coffee directly from more than 800 growers, paying on average 30% more for the highest-quality Arabica beans. We possess a 100% sustainable supply chain.
             </p>
-            <p class="body_txt responsive-padding !pt-0 md:pl-10 !pb-3">
+            <br><br>
+            <h1 class="header_experience_red">
+                UNIVERSITY OF COFFEE
+            </h1>
+            <p class="mt-4 mb-1 text-lg text-gray-700">
                 We prioritize environmentally conscious practices. The establishment of the University of Coffee, ultra-low emission coffee roasting plants, and partnerships with universities to research sustainable coffee production. We pursue sustainability by sharing the value it generates (economic sustainability), personal growth (social sustainability) and respect for the ecosystem (environmental sustainability)
             </p>
-            <button type="submit" class="hidden lg:block w-3/4 p-2 rounded-full bg-red-600 mx-auto text-white font-bold"> Get In Touch</button>
         </div>
 
-        <div class="flex justify-center p-4 mt-16">
-            <img src="{{ asset('images/experience/image_two.png') }}" alt="Coffee" class="w-full h-64 md:h-80 lg:h-96 object-cover">
+        <div class="flex justify-center">
+            <img src="{{ asset('images/experience/image_illy_one.png')}}" alt="Coffee" style="width: 668px;  height: 648px;" class="hidden object-cover lg:block">
         </div>
-        <button type="submit" class="block lg:hidden w-3/4 p-2 rounded-full bg-red-600 mx-auto text-white font-bold"> Get In Touch</button>
+    </div>
+
+    <div class="grid grid-cols-1 gap-4 sm:grid-cols-2" style="background: #F6F0EB">
+        <div class="flex justify-center">
+            <img src="{{ asset('images/experience/image_illy_two.png')}}" alt="Coffee" style="width: 658px;  height: 638px;" class="object-cover">
+        </div>
+
+        <div class="flex flex-col text-left">
+            <br><br><br><br>
+            <h1 class="uppercase header_experience_red">
+                Our Goal is to Delight Coffee Lovers
+            </h1>
+            <p class="mt-4 mb-1 text-lg text-gray-700">
+                Illy aims to be the reference standard for coffee excellence and culture worldwide, an innovative company that offers the best products and places to enjoy them, and thanks to this grows and becomes a leader in high-end coffee.
+            </p>
+            <br><br>
+            <p class="uppercase header_experience_red">
+                We are serious about sustainability
+            </p>
+            <p class="mt-4 mb-1 text-lg text-gray-700">
+                Illy has been carbon neutral since 2023 after creating the “One Makes the Difference” initiative to demonstrate how sustainable quality helps protect and improve the well being of the planet. It reminds consumers that everyone’s actions can make a difference.
+            </p>
+            <br>
+            <div class="flex justify-center">
+                <img src="{{ asset('images/experience/image_illy_one.png')}}" alt="Coffee" style="width: 668px;  height: 648px;" class="block object-cover lg:hidden">
+            </div>
+            <br>
+            <button type="submit" class="hidden w-full p-2 mx-auto font-bold text-white bg-red-600 rounded-full lg:block"> Get In Touch</button>
+        </div>
+        <button type="submit" class="block w-3/4 p-2 mx-auto font-bold text-white bg-red-600 rounded-full lg:hidden"> Get In Touch</button>
 
     </div>
 
-    <div class="grid grid-cols-1 sm:grid-cols-2 gap-4 mt-2">
+    <div class="flex justify-center hidden mt-2 lg:block">
+        <img src="{{ asset('images/experience/image_illy_3.png')}}" alt="Coffee Illy" style="width: 1440px; height: 1150px;" class="object-cover shadow-md">
+    </div>
+
+
+    <div class="grid grid-cols-1 gap-4 mt-2 sm:grid-cols-2">
+
         <div class="mb-2">
-            <img src="images/experience/image_red.png" alt="" class="object-cover">
+            <img src="{{ asset('images/experience/image_red.png')}}" alt="" class="block object-cover lg:hidden">
         </div>
         <div class="mb-2">
-            <img src="images/experience/image_white.png" alt="" class="w-full lg:w-3/4 object-cover">
+            <img src="{{ asset('images/experience/image_white.png')}}" alt="" class="block object-cover w-full lg:hidden lg:w-3/4">
         </div>
     </div>
 </div>
--- resources/views/new/main.blade.php
+++ resources/views/new/main.blade.php
@@ -1,66 +1,60 @@
 <div class="container py-4 mx-auto">
-
-    <div class="grid grid-cols-1 lg:grid-cols-2 gap-4 mt-2">
-        <div class="flex flex-col ">
-            <img src="{{ asset('images/new/new.png') }}" class="hidden lg:block w-3/4 object-cover" alt="Coffee">
+    <div class="grid grid-cols-1 gap-4 mt-2 lg:grid-cols-3">
+        <div class="flex justify-center">
         </div>
-        <div class="flex flex-col text-center mb-8">
-            <span class="header_black_new" >
-                The New, ‘Ready to Drink’, Cold Brew Cans Vending Machine
+        <div class="flex col-span-2">
+            <span class="top-2 header_black_new">
+                The New Cold Brew Vending Machine
             </span>
-            <div class="grid grid-cols-1 lg:grid-cols-2 gap-8 mt-2">
-                <div class="flex flex-col gap-2">
-                    <p class="label_new">
-                        These ready to drink beverages are expertly crafted to offer coffee lovers a refreshing way to enjoy coffee anywhere they want it. The unit features all three illy cold brew varieties in convenient, on-the-go, 8.5oz slim cans. Pick from a wide selection of espresso beverages, delivered quickly and conveniently.
-                    </p>
-                    <br>
-                    <div class="mb-2">
-                        <img src="images/new/FrameA.png" class="hidden lg:block" alt="">
-                    </div>
-                </div>
-                <div class="flex flex-col gap-2">
-                    <p class="label_new">
-                        This machine features a modern visual display, 9” color touchscreen and full motion video. It offers product information, integrated cashless payment, multi-product purchases from a single transaction and digital advertising
-                    </p>
-                    <div class="mb-2 mt-0">
-                        <img src="images/new/illy_mobile.png" class="block lg:hidden object-cover" alt="Coffee">
+        </div>
+    </div>
+    <div class="grid grid-cols-1 gap-4 mt-2 lg:grid-cols-3">
 
-                        <img src="images/new/FrameA.png" class="block lg:hidden object-cover" alt="Frame">
+        <div class="flex justify-center">
+            <img src="{{ asset('images/new/machine_new.png')}}"
+                 class="hidden w-[260px] h-[479px] lg:block"
+                 alt="Coffee">
+        </div>
 
-                        <img src="images/new/FrameB.png" alt="Frame B" class="object-cover">
-                    </div>
-                </div>
+        <div class="flex flex-col justify-center">
+            <p class="body_txt responsive-padding">
+                These ready to drink beverages are expertly crafted to offer coffee lovers a refreshing way to enjoy coffee anywhere they want it. The unit features all three illy cold brew varieties in convenient, on-the-go, 8.5oz slim cans. Pick from a wide selection of espresso beverages, delivered quickly and conveniently.
+            </p>
+            <p class="hidden body_txt responsive-padding lg:block">
+                This machine features a modern visual display, 9” color touchscreen and full motion video. It offers product information, integrated cashless payment, multi-product purchases from a single transaction and digital advertising.
+            </p>
+        </div>
+
+        <div class="flex flex-col items-center">
+            <br>
+            <div class="mb-2 mt-0 lg:mt-4">
+                <img src="{{ asset('images/new/machine_spaces_new.png')}}"
+                     class="hidden w-[401px] h-[169px] lg:block"
+                     alt="">
+            </div>
+            <div class="mb-4 mt-4 p-4">
+
+            </div>
+            <div class="mt-4 mb-2 mt-4">
+                <img src="{{ asset('images/new/electrical_new.png')}}"
+                    class="hidden w-[401px] h-[169px] lg:block"
+                    alt="">
+                <img src="{{ asset('images/new/image_new_mobile.png')}}"
+                     class="block object-cover lg:hidden"
+                     alt="Coffee">
             </div>
         </div>
     </div>
 
-    <div class="text-left mb-8">
-        <h1 class="label_new_premium mb-4">
-            Our Premium Imported Cold Brews
-        </h1>
-        <br class="block lg:hidden">
-    </div>
-    <div class="grid grid-cols-1 sm:grid-cols-3 gap-4 mt-4">
-        <div class="grid grid-cols-2 mt-1">
-            <img src="images/new/base.png" alt="">
-            <div class="card">
-                <p class="header_red">CLASSIC COLD BREW</p>
-                <span class="label_new_end text-sm">Intense coffee aroma enriched with sweet notes of chocolate and caramel</span>
-            </div>
+    <div class="grid grid-cols-1 gap-4 mt-4 bg-white sm:grid-cols-3 ">
+        <div class="grid grid-cols-1 mt-1">
+            <img src="{{ asset('images/new/card_one.png')}}" alt="" class="object-cover w-1/4 h-auto mx-auto lg:w-full lg:h-full">
         </div>
-        <div class="grid grid-cols-2 mt-1">
-            <img src="images/new/base.png" alt="">
-            <div class="card">
-                <p class="header_red">CAPPUCCINO COLD BREW</p>
-                <span class="label_new_end text-sm">Intense coffee aroma enriched with sweet notes of chocolate and caramel</span>
-            </div>
+        <div class="grid grid-cols-1 mt-1">
+            <img src="{{ asset('images/new/card_two.png')}}" alt="" class="object-cover w-1/4 h-auto mx-auto lg:w-full lg:h-full">
         </div>
-        <div class="grid grid-cols-2 mt-1">
-            <img src="images/new/base.png" alt="">
-            <div class="card">
-                <p class="header_red">LATTE macchiato cold brew</p>
-                <span class="label_new_end text-sm">Intense coffee aroma enriched with sweet notes of chocolate and caramel</span>
-            </div>
+        <div class="grid grid-cols-1 mt-1">
+            <img src="{{ asset('images/new/card_three.png')}}" alt="" class="object-cover w-1/4 h-auto mx-auto lg:w-full lg:h-full">
         </div>
     </div>
 </div>
