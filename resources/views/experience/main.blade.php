b3403395030dbb28800e3f9dec0312f91f26ccfc
Marko nava <mnava@prepango.com>
2024-12-11 16:47:08 -0600

    update

--- resources/views/experience/main.blade.php
+++ resources/views/experience/main.blade.php
@@ -1,26 +1,34 @@
 <div class="" style="background: #F6F0EB">
+
     <div class="flex items-center justify-center space-x-2">
         <div class="header_1 text-black hidden lg:block">See What Makes</div>
         <div class="w-12 h-[32px] md:w-[83px] md:h-[73px] bg-no-repeat bg-center bg-contain hidden lg:block" style="background-image: url('/images/illy_red.svg');"></div>
         <div class="header_1 text-black hidden lg:block">an Exceptional Partner</div>
     </div>
-    <div class="mb-8 text-center">
-        <div class="flex justify-center p-2 mt-4">
+
+    <div class="mb-1 text-center">
+        <div class="flex ml-6 mt-1">
             <img src="{{ asset('images/experience/label_illy_mobile.png') }}" class="block lg:hidden" alt="label">
         </div>
     </div>
+
     <div class="grid grid-cols-1 gap-4 mt-2 sm:grid-cols-2" style="background: #F6F0EB">
         <div class="flex flex-col text-left responsive-padding">
             <h1 class="header_experience_red hidden lg:block">
                 THE DIRECT TRADE MODEL
             </h1>
-            <h1 class="text-[#f80000] text-base font-extrabold font-['Manrope'] uppercase leading-normal block lg:hidden">WE AIM TO DELIGHT COFFEE LOVERS</h1>
+            <h1 class="text-[#f80000] text-base font-extrabold font-['Manrope'] uppercase leading-normal block lg:hidden">
+                WE AIM TO DELIGHT COFFEE LOVERS
+            </h1>
+
             <p class="mt-4 mb-1 text-lg text-gray-700 hidden lg:block">
                 Every single bean we harvest is 100% traceable to the source. We purchase our coffee directly from more than 800 growers, paying on average 30% more for the highest-quality Arabica beans. We possess a 100% sustainable supply chain.
             </p>
+
             <p class="text-black text-base font-normal font-['Manrope'] leading-normal block lg:hidden">
-                illy aims to be the reference standard for<br/>coffee excellence and culture worldwide, an<br/>innovative company that offers the best<br/>products and places to enjoy them, and<br/>thanks to this grows and becomes a leader<br/>in high-end coffee.
+                illy aims to be the reference standard for coffee excellence and culture worldwide, an innovative company that offers the best products and places to enjoy them, and thanks to this grows and becomes a leader in high-end coffee.
             </p>
+
             <br><br>
             <h1 class="header_experience_red hidden lg:block">
                 UNIVERSITY OF COFFEE
@@ -34,17 +42,16 @@
             <p class="text-black text-base font-normal font-['Manrope'] leading-normal block lg:hidden">
                 illy has been carbon neutral since 2023 after creating the “One Makes the Difference” initiative to demonstrate how sustainable quality helps protect and improve the well being of the planet. It reminds consumers that everyone’s actions can make a difference.
             </p>
+            <img src="{{ asset('images/experience/image_experience_mobile_one.png')}}" alt="Coffee" class="block lg:hidden mt-2 object-cover">
         </div>
 
-        <div class="flex justify-center responsive-padding hidden lg:block">
+        <div class="flex hidden lg:block">
             <img src="{{ asset('images/experience/image_illy_one.png')}}" alt="Coffee" style="width: 668px;  height: 648px;" class="hidden object-cover lg:block">
         </div>
-        <div class="flex justify-center responsive-padding block lg:hidden">
-            <img src="{{ asset('images/experience/image_experience_mobile_one.png')}}" alt="Coffee" class="object-cover">
-        </div>
+
 
         {{-- Version Mobile --}}
-        <div class="flex flex-col text-left responsive-padding block lg:hidden ">
+        <div class="flex flex-col text-left responsive-padding block lg:hidden -mt-4 lg:mt-0">
             <h1 class="text-[#f80000] text-base font-extrabold font-['Manrope'] uppercase leading-normal ">
                 THE DIRECT TRADE MODEL
             </h1>
@@ -59,11 +66,11 @@
                 We prioritize environmentally conscious practices. The establishment of the University of Coffee, ultra-low emission coffee roasting plants, and partnerships with universities to research sustainable coffee production. We pursue sustainability by sharing the value it generates (economic sustainability), personal growth (social sustainability) and respect for the ecosystem (environmental sustainability)
             </p>
 
-            <div class="flex justify-center responsive-padding">
-                <img src="{{ asset('images/experience/image_experience_mobile_two.png')}}" alt="Coffee" class="object-cover">
+            <div class="flex">
+                <img src="{{ asset('images/experience/image_experience_mobile_two.png')}}" alt="Coffee" class="w-full h-full object-cover">
             </div>
         </div>
-        <button type="submit" class="w-3/4 p-2 mx-auto font-bold text-white bg-red-600 rounded-full"> Get In Touch</button>
+        <button type="submit" class="w-3/4 p-4 mx-auto font-bold text-white bg-red-600 rounded-full"> Get In Touch</button>
     </div>
 
     <div class="grid grid-cols-1 sm:grid-cols-2" style="background: #F6F0EB">
@@ -72,8 +79,7 @@
                 alt="Coffee" style="width: 658px;  height: 638px;"
                 class="object-cover relative -top-72">
         </div>
-        <div class="responsive-padding flex flex-col text-left hidden lg:block">
-            <br>
+        <div class="responsive-padding flex flex-col text-left hidden lg:block -mt-4 p-4">
             <h1 class="uppercase header_experience_red hidden lg:block">
                 Our Goal is to Delight Coffee Lovers
             </h1>
--- resources/views/new/main.blade.php
+++ resources/views/new/main.blade.php
@@ -1,60 +1,73 @@
-<div class="container py-4 mx-auto">
-    <div class="grid grid-cols-1 gap-4 mt-2 lg:grid-cols-3">
-        <div class="flex justify-center">
+<div class="">
+    <span class="p-2 header_black_new block lg:hidden">
+        The New Cold Brew Vending Machine
+    </span>
+    <div class="grid grid-cols-12 gap-4 items-center">
+        <div class="col-span-3">
         </div>
-        <div class="flex col-span-2">
-            <span class="top-2 header_black_new">
+        <div class="col-span-8">
+            <span class="header_black_new hidden lg:block">
                 The New Cold Brew Vending Machine
             </span>
         </div>
     </div>
-    <div class="grid grid-cols-1 gap-4 mt-2 lg:grid-cols-3">
-
-        <div class="flex justify-center">
+    <div class="grid grid-cols-1 lg:grid-cols-12 gap-4 items-center">
+        <!-- Imagen izquierda -->
+        <div class="col-span-3">
             <img src="{{ asset('images/new/machine_new.png')}}"
-                 class="hidden w-[260px] h-[479px] lg:block"
-                 alt="Coffee">
+            class="hidden w-[260px] h-[479px] relative -top-12 p-4 lg:block"
+            alt="Coffee">
         </div>
 
-        <div class="flex flex-col justify-center">
-            <p class="body_txt responsive-padding">
-                These ready to drink beverages are expertly crafted to offer coffee lovers a refreshing way to enjoy coffee anywhere they want it. The unit features all three illy cold brew varieties in convenient, on-the-go, 8.5oz slim cans. Pick from a wide selection of espresso beverages, delivered quickly and conveniently.
-            </p>
-            <p class="hidden body_txt responsive-padding lg:block">
-                This machine features a modern visual display, 9” color touchscreen and full motion video. It offers product information, integrated cashless payment, multi-product purchases from a single transaction and digital advertising.
+        <!-- Texto central -->
+        <div class="col-span-5">
+            <p class="text-black text-xl font-normal font-['Manrope'] leading-[30px] p-4 lg:p-0">
+                These ready to drink beverages are expertly crafted to offer coffee lovers a refreshing way to enjoy coffee anywhere they want it. The unit features all three illy cold brew varieties in convenient, on-the-go, 8.5oz slim cans. Pick from a wide selection of espresso beverages, delivered quickly and conveniently.<br/><br/>This machine features a modern visual display, 9” color touchscreen and full motion video. It offers product information, integrated cashless payment, multi-product purchases from a single transaction and digital advertising
             </p>
         </div>
 
-        <div class="flex flex-col items-center">
-            <br>
-            <div class="mb-2 mt-0 lg:mt-4">
+        <!-- Imagen derecha -->
+        <div class="col-span-3">
+            <div class="mb-2 mt-0">
                 <img src="{{ asset('images/new/machine_spaces_new.png')}}"
                      class="hidden w-[401px] h-[169px] lg:block"
                      alt="">
             </div>
-            <div class="mb-4 mt-4 p-4">
 
-            </div>
             <div class="mt-4 mb-2 mt-4">
                 <img src="{{ asset('images/new/electrical_new.png')}}"
                     class="hidden w-[401px] h-[169px] lg:block"
                     alt="">
                 <img src="{{ asset('images/new/image_new_mobile.png')}}"
-                     class="block w-full h-full object-cover lg:hidden"
+                     class="block object-cover lg:hidden"
                      alt="Coffee">
             </div>
         </div>
     </div>
-
-    <div class="grid grid-cols-1 gap-4 mt-4 bg-white sm:grid-cols-3 ">
-        <div class="grid grid-cols-1 mt-1">
-            <img src="{{ asset('images/new/card_one.png')}}" alt="" class="object-cover w-3/4 h-auto mx-auto lg:w-full lg:h-full">
+    <div class="justify-between items-center flex flex-col sm:flex-row md:inline-flex">
+        <div class="grow shrink basis-0 h-[120px] justify-start items-start flex">
+            <img src="{{ asset('images/new/white.png')}}" alt="" class="object-cover">
+            <div class="grow shrink basis-0 pr-6 flex-col justify-start items-center inline-flex">
+                <span class="self-stretch text-[#f80000] text-xl font-extrabold font-['Manrope'] uppercase leading-[30px]">CLASSIC COLD BREW</span>
+                <p class="self-stretch text-black text-xl font-normal font-['Manrope'] leading-[30px]">
+                    Intense coffee aroma enriched with sweet notes of chocolate and caramel
+                </p>
+            </div>
         </div>
-        <div class="grid grid-cols-1 mt-1">
-            <img src="{{ asset('images/new/card_two.png')}}" alt="" class="object-cover w-3/4 h-auto mx-auto lg:w-full lg:h-full">
+        <div class="grow shrink basis-0 h-[120px] justify-start items-start flex">
+            <img src="{{ asset('images/new/capuccino.png')}}" alt="" class="object-cover">
+            <div class="grow shrink basis-0 pr-6 flex-col justify-start items-center inline-flex">
+                <span class="self-stretch text-[#f80000] text-xl font-extrabold font-['Manrope'] uppercase leading-[30px]">CAPPUCCINO COLD BREW</span>
+                <p class="self-stretch text-black text-xl font-normal font-['Manrope'] leading-[30px]">Smooth cold brew with low fat milk and a hint of cocoa</p>
+            </div>
         </div>
-        <div class="grid grid-cols-1 mt-1">
-            <img src="{{ asset('images/new/card_three.png')}}" alt="" class="object-cover w-3/4 h-auto mx-auto lg:w-full lg:h-full">
+        <div class="grow shrink basis-0 h-[120px] justify-start items-start flex">
+            <img src="{{ asset('images/new/red.png')}}" alt="" class="object-cover">
+            <div class="grow shrink basis-0 self-stretch pr-6 flex-col justify-start items-center inline-flex">
+                <span class="self-stretch text-[#f80000] text-xl font-extrabold font-['Manrope'] uppercase leading-[30px]">LATTE macchiato cold brew</span>
+                <p class="self-stretch text-black text-xl font-normal font-['Manrope'] leading-[30px]">Milk based with a delicate note of caramel and coffee</p>
+            </div>
         </div>
     </div>
+
 </div>
