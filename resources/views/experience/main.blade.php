bc2323d8e01f7ad0cbe64bcbb32ef45c007e2c18
Marko nava <mnava@prepango.com>
2024-12-11 13:52:41 -0600

    upd

--- resources/views/experience/main.blade.php
+++ resources/views/experience/main.blade.php
@@ -1,71 +1,105 @@
-<div class="container py-4 mx-auto" style="background: #F6F0EB">
+<div class="" style="background: #F6F0EB">
+    <div class="flex items-center justify-center space-x-2">
+        <div class="header_1 text-black hidden lg:block">See What Makes</div>
+        <div class="w-12 h-[32px] md:w-[83px] md:h-[73px] bg-no-repeat bg-center bg-contain hidden lg:block" style="background-image: url('/images/illy_red.svg');"></div>
+        <div class="header_1 text-black hidden lg:block">an Exceptional Partner</div>
+    </div>
     <div class="mb-8 text-center">
         <div class="flex justify-center p-2 mt-4">
-            <img src="{{ asset('images/experience/label_illy.png') }}" class="hidden lg:block" alt="label">
             <img src="{{ asset('images/experience/label_illy_mobile.png') }}" class="block lg:hidden" alt="label">
         </div>
     </div>
     <div class="grid grid-cols-1 gap-4 mt-2 sm:grid-cols-2" style="background: #F6F0EB">
-
-        <div class="flex flex-col text-left">
-            <h1 class="header_experience_red">
+        <div class="flex flex-col text-left responsive-padding">
+            <h1 class="header_experience_red hidden lg:block">
                 THE DIRECT TRADE MODEL
             </h1>
-            <p class="mt-4 mb-1 text-lg text-gray-700">
+            <h1 class="text-[#f80000] text-base font-extrabold font-['Manrope'] uppercase leading-normal block lg:hidden">WE AIM TO DELIGHT COFFEE LOVERS</h1>
+            <p class="mt-4 mb-1 text-lg text-gray-700 hidden lg:block">
                 Every single bean we harvest is 100% traceable to the source. We purchase our coffee directly from more than 800 growers, paying on average 30% more for the highest-quality Arabica beans. We possess a 100% sustainable supply chain.
             </p>
+            <p class="text-black text-base font-normal font-['Manrope'] leading-normal block lg:hidden">
+                illy aims to be the reference standard for<br/>coffee excellence and culture worldwide, an<br/>innovative company that offers the best<br/>products and places to enjoy them, and<br/>thanks to this grows and becomes a leader<br/>in high-end coffee.
+            </p>
             <br><br>
-            <h1 class="header_experience_red">
+            <h1 class="header_experience_red hidden lg:block">
                 UNIVERSITY OF COFFEE
             </h1>
-            <p class="mt-4 mb-1 text-lg text-gray-700">
+            <h1 class="block lg:hidden text-[#f80000] text-base font-extrabold font-['Manrope'] uppercase leading-normal">
+                We are serious about sustainability
+            </h1>
+            <p class="mt-4 mb-1 text-lg text-gray-700 hidden lg:block">
                 We prioritize environmentally conscious practices. The establishment of the University of Coffee, ultra-low emission coffee roasting plants, and partnerships with universities to research sustainable coffee production. We pursue sustainability by sharing the value it generates (economic sustainability), personal growth (social sustainability) and respect for the ecosystem (environmental sustainability)
+            </p>
+            <p class="text-black text-base font-normal font-['Manrope'] leading-normal block lg:hidden">
+                illy has been carbon neutral since 2023 after creating the “One Makes the Difference” initiative to demonstrate how sustainable quality helps protect and improve the well being of the planet. It reminds consumers that everyone’s actions can make a difference.
             </p>
         </div>
 
-        <div class="flex justify-center">
+        <div class="flex justify-center responsive-padding hidden lg:block">
             <img src="{{ asset('images/experience/image_illy_one.png')}}" alt="Coffee" style="width: 668px;  height: 648px;" class="hidden object-cover lg:block">
         </div>
-    </div>
-
-    <div class="grid grid-cols-1 gap-4 sm:grid-cols-2" style="background: #F6F0EB">
-        <div class="flex justify-center">
-            <img src="{{ asset('images/experience/image_illy_two.png')}}" alt="Coffee" style="width: 658px;  height: 638px;" class="object-cover">
+        <div class="flex justify-center responsive-padding block lg:hidden">
+            <img src="{{ asset('images/experience/image_experience_mobile_one.png')}}" alt="Coffee" class="object-cover">
         </div>
 
-        <div class="flex flex-col text-left">
-            <br><br><br><br>
-            <h1 class="uppercase header_experience_red">
+        {{-- Version Mobile --}}
+        <div class="flex flex-col text-left responsive-padding block lg:hidden ">
+            <h1 class="text-[#f80000] text-base font-extrabold font-['Manrope'] uppercase leading-normal ">
+                THE DIRECT TRADE MODEL
+            </h1>
+            <p class="text-black text-base font-normal font-['Manrope'] leading-normal">
+                Every single bean we harvest is 100% traceable to the source. We purchase our coffee directly from more than 800 growers, paying on average 30% more for the highest-quality Arabica beans. We possess a 100% sustainable supply chain.
+            </p>
+            <br>
+            <h1 class="text-[#f80000] text-base font-extrabold font-['Manrope'] uppercase leading-normal">
+                UNIVERSITY OF COFFEE
+            </h1>
+            <p class="text-black text-base font-normal font-['Manrope'] leading-normal">
+                We prioritize environmentally conscious practices. The establishment of the University of Coffee, ultra-low emission coffee roasting plants, and partnerships with universities to research sustainable coffee production. We pursue sustainability by sharing the value it generates (economic sustainability), personal growth (social sustainability) and respect for the ecosystem (environmental sustainability)
+            </p>
+
+            <div class="flex justify-center responsive-padding">
+                <img src="{{ asset('images/experience/image_experience_mobile_two.png')}}" alt="Coffee" class="object-cover">
+            </div>
+        </div>
+        <button type="submit" class="w-3/4 p-2 mx-auto font-bold text-white bg-red-600 rounded-full"> Get In Touch</button>
+    </div>
+
+    <div class="grid grid-cols-1 sm:grid-cols-2" style="background: #F6F0EB">
+        <div class="responsive-padding flex justify-center relative hidden lg:block">
+            <img src="{{ asset('images/experience/image_illy_two.png')}}"
+                alt="Coffee" style="width: 658px;  height: 638px;"
+                class="object-cover relative -top-72">
+        </div>
+        <div class="responsive-padding flex flex-col text-left hidden lg:block">
+            <br>
+            <h1 class="uppercase header_experience_red hidden lg:block">
                 Our Goal is to Delight Coffee Lovers
             </h1>
-            <p class="mt-4 mb-1 text-lg text-gray-700">
+            <p class="mt-4 mb-1 text-lg text-gray-700 hidden lg:block">
                 Illy aims to be the reference standard for coffee excellence and culture worldwide, an innovative company that offers the best products and places to enjoy them, and thanks to this grows and becomes a leader in high-end coffee.
             </p>
             <br><br>
-            <p class="uppercase header_experience_red">
+            <p class="uppercase header_experience_red hidden lg:block">
                 We are serious about sustainability
             </p>
-            <p class="mt-4 mb-1 text-lg text-gray-700">
+            <p class="mt-4 mb-1 text-lg text-gray-700 hidden lg:block">
                 Illy has been carbon neutral since 2023 after creating the “One Makes the Difference” initiative to demonstrate how sustainable quality helps protect and improve the well being of the planet. It reminds consumers that everyone’s actions can make a difference.
             </p>
             <br>
-            <div class="flex justify-center">
+            <div class="flex justify-center hidden lg:block">
                 <img src="{{ asset('images/experience/image_illy_one.png')}}" alt="Coffee" style="width: 668px;  height: 648px;" class="block object-cover lg:hidden">
             </div>
             <br>
-            <button type="submit" class="hidden w-full p-2 mx-auto font-bold text-white bg-red-600 rounded-full lg:block"> Get In Touch</button>
+            <button type="submit" class="w-full p-2 mx-auto font-bold text-white bg-red-600 rounded-full hidden lg:block"> Get In Touch</button>
         </div>
-        <button type="submit" class="block w-3/4 p-2 mx-auto font-bold text-white bg-red-600 rounded-full lg:hidden"> Get In Touch</button>
-
     </div>
 
-    <div class="flex justify-center hidden mt-2 lg:block">
+    <div class="flex justify-center hidden lg:block -mt-56">
         <img src="{{ asset('images/experience/image_illy_3.png')}}" alt="Coffee Illy" style="width: 1440px; height: 1150px;" class="object-cover shadow-md">
     </div>
-
-
     <div class="grid grid-cols-1 gap-4 mt-2 sm:grid-cols-2">
-
         <div class="mb-2">
             <img src="{{ asset('images/experience/image_red.png')}}" alt="" class="block object-cover lg:hidden">
         </div>
