eeee7138fbfbe3959eb59ab2b5ad9bd1b2ae8006
Brigette Gutierrez <brigette@prepango.com>
2024-12-11 12:36:29 -0600

    machine

--- resources/views/machine/main.blade.php
+++ resources/views/machine/main.blade.php
@@ -56,7 +56,9 @@
         </div>
 
         <div class="flex justify-center order-2 sm:order-1">
-            <div class="grid grid-cols-1 sm:grid-cols-3 flex flex-col items-center justify-center">
+
+
+            <!-- <div class="grid grid-cols-1 sm:grid-cols-3 flex flex-col items-center justify-center">
                 <div clas="grid grid-cols-2 flex flex-col items-center">
                 <div class="flex flex-col items-center">
                     <img class="Image  w-full h-[130px]  md:h-[168px] relative"src="{{ asset('images/machine/cup_1.png') }}" alt="Coffee Machine" />
@@ -82,10 +84,145 @@
                     </div>
                 </div>
 
-            </div>
+            </div> -->
 
         </div>
 
 
     </div>
+    <div class="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-3 gap-6 px-4">
+             <!-- Item 1 -->
+            <div class="flex flex-col items-center text-center">
+                <div class="grid grid-cols-2 gap-6 px-4">
+                    <div class="pr-5 flex flex-col items-center justify-center">
+                        <img class="w-[120px] h-[120px] object-cover" src="{{ asset('images/machine/cup_1.png') }}" alt="Drip Coffee">
+                    </div>
+
+                    <div class="text-left ">
+                    <h3 class="header_red !p-0 text-left">DRIP COFFEE</h3>
+                    <p class="body_txt">Illy coffee prepared by the filter drip method, served in a 12oz cup</p>
+                    </div>
+
+                </div>
+
+            </div>
+
+            <!-- Item 2 -->
+            <div class="flex flex-col items-center text-center">
+                 <div class="grid grid-cols-2 gap-6 px-4">
+                    <div class="pr-5 flex flex-col items-center justify-center">
+                    <img class="w-[120px] h-[120px] object-cover" src="{{ asset('images/machine/cup_2.png') }}" alt="Cappuccino">
+                </div>
+
+                    <div class="text-left ">
+                        <h3 class="header_red !p-0 text-left">CAPPUCCINO</h3>
+                        <p class="body_txt">6oz espresso blended with steamed and frothed milk, served in a 12oz cup</p>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Item 3 -->
+            <div class="flex flex-col items-center text-center">
+                 <div class="grid grid-cols-2 gap-6 px-4">
+                    <div class="pr-5 flex flex-col items-center justify-center">
+                    <img class="w-[120px] h-[120px] object-cover" src="{{ asset('images/machine/cup_3.png') }}" alt="Cappuccino">
+                </div>
+
+                    <div class="text-left ">
+                        <h3 class="header_red !p-0 text-left">CAFFÈ LATTE</h3>
+                        <p class="body_txt">Espresso mixed with steamed milk, served in a 12oz cup</p>
+                    </div>
+                </div>
+            </div>
+
+               <!-- Item 1 -->
+               <div class="flex flex-col items-center text-center">
+                 <div class="grid grid-cols-2 gap-6 px-4">
+                    <div class="pr-5 flex flex-col items-center justify-center">
+                    <img class="w-[120px] h-[120px] object-cover" src="{{ asset('images/machine/cup_4.png') }}" alt="Drip Coffee">
+                </div>
+                    <div class="text-left ">
+                    <h3 class="header_red !p-0 text-left">CAFFÈ MOCHAE</h3>
+                    <p class="body_txt">Espresso blended with thick chocolate and steamed milk, served in a 12oz cup</p>
+                    </div>
+
+                </div>
+
+            </div>
+
+            <!-- Item 2 -->
+            <div class="flex flex-col items-center text-center">
+                 <div class="grid grid-cols-2 gap-6 px-4">
+                    <div class="pr-5 flex flex-col items-center justify-center">
+                    <img class="w-[120px] h-[120px] object-cover" src="{{ asset('images/machine/cup_5.png') }}" alt="Cappuccino">
+                </div>
+
+                    <div class="text-left ">
+                        <h3 class="header_red !p-0 text-left">espresso</h3>
+                        <p class="body_txt">2oz of fresh illy espresso beans prepared in its purest form, served in a 12oz cup</p>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Item 3 -->
+            <div class="flex flex-col items-center text-center">
+                 <div class="grid grid-cols-2 gap-6 px-4">
+                    <div class="pr-5 flex flex-col items-center justify-center">
+                    <img class="w-[120px] h-[120px] object-cover" src="{{ asset('images/machine/cup_6.png') }}" alt="Cappuccino">
+                </div>
+
+                    <div class="text-left ">
+                        <h3 class="header_red !p-0 text-left">double esresso</h3>
+                        <p class="body_txt">E4oz of fresh illy espresso beans prepared in its purest form, served in a 12oz cup</p>
+                    </div>
+                </div>
+            </div>
+
+
+               <!-- Item 1 -->
+               <div class="flex flex-col items-center text-center">
+                 <div class="grid grid-cols-2 gap-6 px-4">
+                    <div class="pr-5 flex flex-col items-center justify-center">
+                    <img class="w-[120px] h-[120px] object-cover" src="{{ asset('images/machine/cup_7.png') }}" alt="Drip Coffee">
+                </div>
+                    <div class="text-left ">
+                    <h3 class="header_red !p-0 text-left">macchiato</h3>
+                    <p class="body_txt">2oz of fresh illy espresso beans with a touch of frothed milk, served in a 12oz cup</p>
+                    </div>
+
+                </div>
+
+            </div>
+
+            <!-- Item 2 -->
+            <div class="flex flex-col items-center text-center">
+                 <div class="grid grid-cols-2 gap-6 px-4">
+                    <div class="pr-5 flex flex-col items-center justify-center">
+                    <img class="w-[120px] h-[120px] object-cover" src="{{ asset('images/machine/cup_8.png') }}" alt="Cappuccino">
+                </div>
+
+                    <div class="text-left ">
+                        <h3 class="header_red !p-0 text-left">chocolate</h3>
+                        <p class="body_txt">Thick chocolate and steamed milk, served in a 12oz cup</p>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Item 3 -->
+            <div class="flex flex-col items-center text-center">
+                 <div class="grid grid-cols-2 gap-6 px-4">
+                    <div class="pr-5 flex flex-col items-center justify-center">
+                    <img class="w-[120px] h-[120px] object-cover" src="{{ asset('images/machine/cup_9.png') }}" alt="Cappuccino">
+                </div>
+
+                    <div class="text-left ">
+                        <h3 class="header_red !p-0 text-left">Americano</h3>
+                        <p class="body_txt">Espresso & hot water, matching the brewed coffee strength. Served in a 12oz cup</p>
+                    </div>
+                </div>
+            </div>
+
+
+
+        </div>
 </div>
