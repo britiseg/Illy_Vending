2868bca51ac24fa480d6ba74d37fb18cbe7f2a7a
Marko nava <mnava@prepango.com>
2024-12-19 18:41:01 -0600

    Adjust final

--- resources/views/new/main.blade.php
+++ resources/views/new/main.blade.php
@@ -1,50 +1,51 @@
 <div class="">
-    <span class="responsive-padding header_black_new block lg:hidden">
-        The New Cold Brew Vending Machine
-    </span>
+    <div class="block lg:hidden">
+        <span class="responsive-padding header_black_new block lg:hidden">
+            The New Cold Brew Vending Machine
+        </span>
+        <p class="body_txt responsive-padding -mt-4">
+            These ready to drink beverages are expertly crafted to offer coffee lovers a refreshing way to enjoy coffee anywhere they want it. The unit features all three illy cold brew varieties in convenient, on-the-go, 8.5oz slim cans. Pick from a wide selection of espresso beverages, delivered quickly and conveniently.
+        </p>
+    </div>
+    <div class="hidden lg:flex flex-col lg:flex-row items-start lg:gap-12 p-4 w-full h-auto">
+        <img src="{{ asset('images/new/machine_new.png') }}"
+             class="w-[260px] h-[479px] p-8 object-cover"
+             alt="Coffee">
 
-    <div class="grid grid-cols-12 gap-4 items-center">
-        <div class="col-span-3">
-        </div>
-        <div class="col-span-8">
-            <span class="header_black_new hidden lg:block">
+        <div class="hidden lg:flex flex-col gap-4 lg:gap-6 w-full">
+            <span class="text-black text-[52px] font-bold leading-[78px] font-[Manrope]">
                 The New Cold Brew Vending Machine
             </span>
-        </div>
-    </div>
-
-    <div class="grid grid-cols-1 lg:grid-cols-12 gap-4 items-center">
-        <div class="col-span-3">
-            <img src="{{ asset('images/new/machine_new.png')}}"
-            class="hidden w-[260px] h-[479px] relative -top-12 p-4 lg:block"
-            alt="Coffee">
-        </div>
-        <div class="col-span-5">
-            <p class="text-black text-xl font-normal font-['Manrope'] leading-[30px] responsive-padding lg:p-0">
-                These ready to drink beverages are expertly crafted to offer coffee lovers a refreshing way to enjoy coffee anywhere they want it. The unit features all three illy cold brew varieties in convenient, on-the-go, 8.5oz slim cans. Pick from a wide selection of espresso beverages, delivered quickly and conveniently.<br/><br/>This machine features a modern visual display, 9” color touchscreen and full motion video. It offers product information, integrated cashless payment, multi-product purchases from a single transaction and digital advertising
-            </p>
-        </div>
-        <div class="col-span-3">
-            <div class="mb-2 mt-0 hidden lg:block">
-                <img src="{{ asset('images/new/new_machine.svg')}}"
-                     class="hidden w-[801px] h-[340px] lg:block"
-                     alt="">
+            <div class="flex flex-col lg:flex-row gap-12 items-start">
+                <div class="flex flex-col lg:gap-12 lg:w-[504px]">
+                    <span class="text-black text-[20px] font-[Manrope] font-normal leading-[30px]">
+                        These ready to drink beverages are expertly crafted to offer coffee lovers a refreshing way to enjoy coffee anywhere they want it. The unit features all three illy cold brew varieties in convenient, on-the-go, 8.5oz slim cans. Pick from a wide selection of espresso beverages, delivered quickly and conveniently.
+                        <br/><br/>
+                        This machine features a modern visual display, 9” color touchscreen and full motion video. It offers product information, integrated cashless payment, multi-product purchases from a single transaction and digital advertising.
+                    </span>
+                </div>
+                <div class="hidden lg:flex flex-col justify-between w-[401px] p-2">
+                    <img src="{{ asset('images/new/new_machine.svg') }}"
+                         class="w-full h-auto object-contain"
+                         alt="Cold Brew Machine">
+                </div>
             </div>
         </div>
     </div>
-    <div class="grid grid-cols-2 items-center responsive-padding block lg:hidden">
+
+    <div class="grid grid-cols-2 items-center responsive-padding block lg:hidden -mt-4">
         <div class="flex">
             <img src="{{ asset('images/new/machine_new.png')}}"
-            class="object-cover"
-            alt="Coffee">
+                 class="h-[280px] w-auto object-contain"
+                 alt="Coffee">
         </div>
         <div class="flex">
             <img src="{{ asset('images/new/new_machine.svg')}}"
-                 class="object-cover"
-                 alt="Coffee">
+            class="h-[320px] w-auto object-contain"
+            alt="Coffee">
         </div>
-
     </div>
+
     <div class="justify-between items-center flex flex-col sm:flex-row md:inline-flex">
         <div class="grow shrink basis-0 h-[120px] justify-start items-start flex">
             <img src="{{ asset('images/new/white.png')}}" alt="" class="object-cover">
@@ -55,6 +56,7 @@
                 </p>
             </div>
         </div>
+        <br>
         <div class="grow shrink basis-0 h-[120px] justify-start items-start flex">
             <img src="{{ asset('images/new/capuccino.png')}}" alt="" class="object-cover">
             <div class="grow shrink basis-0 pr-6 flex-col justify-start items-center inline-flex">
